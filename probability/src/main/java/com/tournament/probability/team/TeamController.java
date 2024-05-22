4c11e31ca31c37165b0df0cf9e2651e8ad3dbb62
Krzpiczek <krzysiek12-2000@o2.pl>
2024-05-22 14:04:43 +0200

    Package Team was restructured- one Repo, Controller and Service.

---
+++ probability/src/main/java/com/tournament/probability/team/TeamController.java
@@ -0,0 +1,41 @@
+package com.tournament.probability.team;
+
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "/api/v1/team")
+public class TeamController {
+    @Autowired
+    private final TeamService teamService;
+
+    TeamController(TeamService teamService){
+        this.teamService = teamService;
+    }
+    @GetMapping(path = "/get/all")
+    public List<Team> getAllTeams(){
+        return teamService.getAllTeams();
+    }
+    @GetMapping(path = "/get/{name}")
+    public ResponseEntity<Optional<Team>> getTeam(@PathVariable String name){
+        Optional<Team> team = teamService.getTeamByName(name);
+        return new ResponseEntity<>(team, HttpStatus.OK);
+    }
+    @PostMapping(path = "/add")
+    public ResponseEntity<Team> addTeamMethod(@RequestBody Team team) {
+        Team createdTeam = teamService.addTeam(team);
+        return new ResponseEntity<>(createdTeam, HttpStatus.CREATED);
+    }
+    @Transactional
+    @DeleteMapping(path = "/delete/{id}")
+    public ResponseEntity<Team> deleteTeamById(@PathVariable("id") int id){
+        teamService.deleteTeamById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
---
+++ probability/src/main/java/com/tournament/probability/team/TeamService.java
@@ -0,0 +1,37 @@
+package com.tournament.probability.team;
+
+import com.tournament.probability.exception.ApiRequestException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TeamService {
+    @Autowired
+    private final TeamRepository teamRepository;
+
+    TeamService(TeamRepository teamRepository){
+        this.teamRepository = teamRepository;
+    }
+    List<Team> getAllTeams(){
+        return teamRepository.findAll();
+    }
+    Optional<Team> getTeamByName(String name){
+        Optional<Team> optionalTeam = teamRepository.findTeamByName(name);
+        if(optionalTeam.isEmpty()){
+            throw new ApiRequestException("Team '" + name + "' does not exist.");
+        }
+        return teamRepository.findTeamByName(name);
+    }
+    Team addTeam(Team team){
+        Optional<Team> teamOptional = teamRepository.findTeamByName(team.getName());
+        if(teamOptional.isPresent()){
+            throw new IllegalStateException("Team with given name already exist. Please provide different Team name.");
+        }
+        return teamRepository.save(team);
+    }
+    void deleteTeamById(int id){
+        teamRepository.deleteTeamById(id);
+    }
+}
